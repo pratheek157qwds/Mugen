4bfa8dcdb1a8ba7728ce556e141c099ff71cb788
pratheek157qwds <154472124+pratheek157qwds@users.noreply.github.com>
2024-11-14 00:49:02 +0530

    chages

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    client_token: "MTI0NTMyMDI1OTIxNzUyMjc1OA.G4bY7b.5EceRfTKY9VeaqvPq5xvPz6exMXAYvvl7tmnM0",
-    client_id: "1245320259217522758",
+    client_token: "",
+    client_id: "",
     client_prefix: ".",
-    mongodb_url: "mongodb+srv://j2chb:6q4jtc@cluster0.mmfongd.mongodb.net/?retryWrites=true&w=majority", //optional
+    mongodb_url: "", //optional
     developers: ["744557711513092098"],
     sharding: true,
     database: false,
